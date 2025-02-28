d3dc503859a39169e20b0473247cf070a787c519
diegosenarruzza <diegosenarruzza@gmail.com>
2025-02-28 14:22:31 -0300

    WIP prueba

--- run_coordination_2025.py
+++ run_coordination_2025.py
@@ -2,6 +2,8 @@ from coordination_2025.case_studies.c_pop_1 import e_cfsm as e_cfsm_1
 from coordination_2025.case_studies.c_pop_2 import e_cfsm as e_cfsm_2
 import time
 
+print("Running bisimulation test with models C_pop and C'_pop. At the end, the seconds it took are printed.")
+
 start = time.time()
 
 relation, matches = e_cfsm_1.calculate_bisimulation_with(c_pop_2)
